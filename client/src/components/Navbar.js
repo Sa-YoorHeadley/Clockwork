289ab4c5332744092682d2e45a543958f660fad7
unknown <danielfrollins@gmail.com>
2022-03-03 12:36:36 -0600

    Testing push and changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Accounts from './Accounts'
 export default function Navbar({ handleClick }) {
     
     return (
         <nav className='navbar'>
+            {/* <Accounts /> */}
             <button className='btn' onClick={() => handleClick('create')}>New Employee</button>
             <button className='btn' onClick={() => handleClick('update')}>Update Employee</button>
             <button className='btn' onClick={() => handleClick('delete')}>Delete Employee</button>
