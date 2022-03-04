1757c6f21ad5127ad5c0c3f4e29e258b12f8fe7f
Sa Yoor Headley <sai.h152015@gmail.com>
2022-03-04 10:04:17 -0400

    Updated styling of Accounts.js

--- client/src/components/Accounts.js
+++ client/src/components/Accounts.js
@@ -8,11 +8,14 @@ export default function Accounts({changeDatabase}) {
   }
 
     return (
-    <select onChange={handleChange}>
-        <option value = "default"></option>
-        <option value = "jeffersonHealthDb">Jefferson Health</option>
-        <option value = "optumDb">Optum</option>
-        <option value = "paychexDb">Paychex</option>
-    </select>
+        <div className='accounts'>
+            <h1 className='accounts-header'>Select Database</h1>
+            <select onChange={handleChange} className='accounts-select'>
+                <option value = "default"></option>
+                <option value = "jeffersonHealthDb">Jefferson Health</option>
+                <option value = "optumDb">Optum</option>
+                <option value = "paychexDb">Paychex</option>
+            </select>
+        </div>
   )
 }
