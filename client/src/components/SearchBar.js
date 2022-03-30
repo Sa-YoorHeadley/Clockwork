7a209330b0005934e9c19a3b3fc3477e70bdd121
unknown <danielfrollins@gmail.com>
2022-03-30 12:49:28 -0500

    pulling

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -5,20 +5,25 @@ export default function SearchBar({setSearchKey, setSearchOption,dataKeys}) {
     var genericFilter = {"Name":'', "Location":''}
     getFilterValues( dataKeys)
     console.log(JSON.stringify(genericFilter))
-    const optionElements = dataKeys.map(element=> {
+    const optionElements = dataKeys.map(element,i=> {
         return(
              
-               <option key={element} value = {element}>{element}</option>
+               <option key={element} value = {element}>{genericFilter[index]}</option>
         )
     })
     function getFilterValues(dataKeys){
-      for (var f in genericFilter) {
-        console.log(genericFilter)
-        var genericFilterKey = genericFilter[f].toString()
+      var ok = Object.keys(genericFilter)
+      
+      for (var f = 0 ; f< ok.length; f++) {
+        
+        var genericFilterKey = ok[f]
         for (var dk in dataKeys){
           var dataKeyString = dataKeys[dk].toString()
+        
           console.log(dataKeyString + "<<>>"+ genericFilterKey)
-          if (dataKeyString.toLowerCase().search(genericFilterKey.toLowerCase())) genericFilter[genericFilterKey] = dataKeyString
+          if (dataKeyString.toLocaleLowerCase().search(genericFilterKey.toLocaleLowerCase()) > -1 ) {
+            console.log("Match" + dataKeyString + "<<>>"+ genericFilterKey )
+            genericFilter[genericFilterKey] = genericFilter[genericFilterKey] + "candidate." + dataKeyString +".tostring() + "}
         // if (dataKeys.toString().search(genericFilter[f].key().toLowercase())) genericFilter[genericFilter[f]] = genericFilter[genericFilter[f]] + " " +element
         }
       }
